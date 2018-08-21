014b4cc758a4c411282b59f41b2c7c65c9d30226
ezoran <evanzoran17@gmail.com>
2018-08-21 11:48:49 -0600

    more animation work done
    movement = ok
    scaling = work on

--- public/javascript/animate.js
+++ public/javascript/animate.js
@@ -1,23 +1,54 @@
 var obj = document.getElementById("skater");
-var xPos = 100;
-var yPos = 300;
 
-var width = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
+var xPos; //skate x coordinate
+var yPos; //skater y coordinate
 
-xPos = (width / 2);
+var scale = obj.height; //scale of skater
 
-obj.style.position = "absolute";
-obj.style.left = xPos + 'px';
-obj.style.top = yPos + 'px';
+obj.style.position = "fixed"; //set positioning of skater
 
-
-function resetAnimation()
-{
-}
+var loop = setInterval(playAnimation, 10);
 
 function playAnimation()
 {
+
+  if(xPos <= 0 || xPos == null)
+  {
+    resetAnimation();
+  }
+  else
+  {
+    //loop animation\
+    handleMovement();
+    handleSize();
+  }
+}
+
+function handleMovement()
+{
+  xPos--;
+  yPos++;
+
+  obj.style.left = xPos + 'px';
+  obj.style.bottom = yPos + 'px';
+}
+
+function handleSize()
+{
+  scale--;
+  obj.height = scale;
+}
+
+function resetAnimation()
+{
+  var width = window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+
+  xPos = (width / 2);
+  yPos = -200;
+
+  obj.style.left = xPos + 'px';
+  obj.style.bottom = yPos + 'px';
 
 }
