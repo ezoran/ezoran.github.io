ccefe530830aeee5990432e67907ae158f0a70dd
Evan Zoran <evanzoran@Evans-MacBook-Air.local>
2018-08-22 15:15:38 -0600

    more work on animation

--- public/javascript/animate.js
+++ public/javascript/animate.js
@@ -1,33 +1,54 @@
+//width of the document
+var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var turningPoint = width * (.1); //set the skater's turning point to a certain
+                                 //percentage of the screen
+
+
 var obj = document.getElementById("skater");
 
 var xPos; //skate x coordinate
 var yPos; //skater y coordinate
 
 var scale = obj.height; //scale of skater
+var orig_scale = obj.height; //save original scale of skater
 
 obj.style.position = "fixed"; //set positioning of skater
 
 var loop = setInterval(playAnimation, 10);
 
+
 function playAnimation()
 {
 
-  if(xPos <= 0 || xPos == null)
+  if(xPos <= -100 || xPos == null)
   {
-    resetAnimation();
+    resetMovement();
+    //resetSize();
   }
   else
   {
-    //loop animation\
+    //loop animation
     handleMovement();
-    handleSize();
+  //  handleSize();
   }
 }
 
 function handleMovement()
 {
-  xPos--;
-  yPos++;
+
+  if(xPos >= turningPoint)
+  {
+    xPos--;
+    yPos++;
+  }
+  else
+  {
+    xPos--;
+  }
+
 
   obj.style.left = xPos + 'px';
   obj.style.bottom = yPos + 'px';
@@ -37,13 +58,11 @@ function handleSize()
 {
   scale--;
   obj.height = scale;
+
 }
 
-function resetAnimation()
+function resetMovement()
 {
-  var width = window.innerWidth
-  || document.documentElement.clientWidth
-  || document.body.clientWidth;
 
   xPos = (width / 2);
   yPos = -200;
@@ -51,4 +70,9 @@ function resetAnimation()
   obj.style.left = xPos + 'px';
   obj.style.bottom = yPos + 'px';
 
+}
+
+function resetSize()
+{
+  obj.height = orig_scale;
 }
